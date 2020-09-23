2f7ee06e04fa904eb84ede578cf8473e4c81eeeb
Jakub Cieniewski <jakub.cieniewski@gmail.com>
2020-09-23 23:48:28 +0200

    lego-sw-store

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,32 +10,66 @@ export default class Cart extends Component {
       email: "",
       address: "",
       showCheckout: false,
+      textToCopy: "",
     };
   }
   handleInput = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  createOrder = (e) => {
-    e.preventDefault();
-    const order = {
-      name: this.state.name,
-      email: this.state.email,
-      addresss: this.state.address,
-      cartItems: this.props.cartItems,
-    };
-    this.props.createOrder(order);
+  clearOrder = () => {
+    localStorage.setItem("cartItems", []);
+    window.location.reload(false);
+  };
+  createCopy = (cartItems) => {
+    let text = "";
+    cartItems.forEach((item) => {
+      text +=
+        item.count +
+        "x " +
+        item._id +
+        " " +
+        item.title +
+        " " +
+        formatCurrency(item.price) +
+        " " +
+        "\n";
+    });
+    text +=
+      "Wartość zamówienia: " +
+      formatCurrency(cartItems.reduce((a, c) => a + c.price * c.count, 0));
+    this.setState({ textToCopy: text });
+    this.copyToClipboard(text);
+  };
+  copyToClipboard = (text) => {
+    console.log(text);
+    var textField = document.createElement("textarea");
+    textField.innerText = text;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand("copy");
+    textField.remove();
   };
   render() {
     const { cartItems } = this.props;
+    const textToCopy = this.state.textToCopy;
     return (
       <div>
         {cartItems.length === 0 ? (
-          <div className="cart cart-header"> Cart is empty </div>
+          <div className="cart cart-header"> Zamówienie jest puste </div>
         ) : (
           <div className="cart cart-header">
-            You have {cartItems.length} in the cart{" "}
+            Masz {cartItems.length} pozycj
+            {cartItems.length < 5 ? (cartItems.length > 1 ? "e" : "ę") : "i"} w
+            zamówieniu
+            <button
+              className="clear-order button"
+              onClick={() => this.clearOrder()}
+            >
+              x
+            </button>
           </div>
         )}
+
         <div>
           <div className="cart">
             <Fade left cascade>
@@ -45,7 +79,7 @@ export default class Cart extends Component {
                     <div>
                       <img src={item.image} alt={item.title}></img>
                     </div>
-                    <div>
+                    <div className="max-width">
                       <div>{item.title}</div>
                       <div className="right">
                         {formatCurrency(item.price)} x {item.count}{" "}
@@ -53,7 +87,7 @@ export default class Cart extends Component {
                           className="button"
                           onClick={() => this.props.removeFromCart(item)}
                         >
-                          Remove
+                          Usuń
                         </button>
                       </div>
                     </div>
@@ -67,7 +101,7 @@ export default class Cart extends Component {
               <div className="cart">
                 <div className="total">
                   <div>
-                    Total:{" "}
+                    Suma:{" "}
                     {formatCurrency(
                       cartItems.reduce((a, c) => a + c.price * c.count, 0)
                     )}
@@ -75,52 +109,48 @@ export default class Cart extends Component {
                   <button
                     onClick={() => {
                       this.setState({ showCheckout: true });
+                      this.createCopy(cartItems);
                     }}
                     className="button primary"
                   >
-                    Proceed
+                    Stwórz zamówienie
                   </button>
                 </div>
               </div>
               {this.state.showCheckout && (
                 <Fade right cascade>
-                  <div className="cart">
-                    <form onSubmit={this.createOrder}>
-                      <ul className="form-container">
-                        <li>
-                          <label>Email</label>
-                          <input
-                            name="email"
-                            type="email"
-                            required
-                            onChange={this.handleInput}
-                          ></input>
+                  <div className="ins">
+                    <p>
+                      Dziękujemy za utworzenie zamówienia! Skopiuj proszę
+                      poniższy tekst do wiadomości prywatnej lub w komentarzu
+                      pod postem :)
+                    </p>
+                  </div>
+                  <div className="cart order-items">
+                    <ul className="cart-items">
+                      {cartItems.map((item) => (
+                        <li key={item._id}>
+                          <div className="max-width">
+                            <div>
+                              {item.count}x {item._id} {item.title}{" "}
+                              {formatCurrency(item.price)}
+                            </div>
+                          </div>
                         </li>
-                        <li>
-                          <label>Name</label>
-                          <input
-                            name="name"
-                            type="text"
-                            required
-                            onChange={this.handleInput}
-                          ></input>
-                        </li>
-                        <li>
-                          <label>Address</label>
-                          <input
-                            name="address"
-                            type="text"
-                            required
-                            onChange={this.handleInput}
-                          ></input>
-                        </li>
-                        <li>
-                          <button className="button primary" type="submit">
-                            Checkout
-                          </button>
-                        </li>
-                      </ul>
-                    </form>
+                      ))}
+                      <p>
+                        Wartość zamówienia:{" "}
+                        {formatCurrency(
+                          cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                        )}
+                      </p>
+                      <button
+                        className="button"
+                        onClick={() => this.copyToClipboard(textToCopy)}
+                      >
+                        Skopiuj do schowka
+                      </button>
+                    </ul>
                   </div>
                 </Fade>
               )}
